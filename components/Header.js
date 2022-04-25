18831633aba023623364bb5f0d78f4155644f5c1
PhilippRomanovskiy <philippromanovskiy@gmail.com>
2022-04-25 12:46:18 +0100

    Interface and wallet

---
+++ components/Header.js
@@ -0,0 +1,86 @@
+import React from 'react' 
+import styled from 'styled-components'
+
+
+const Header = ({walletAddress, connectWallet}) => { 
+  return (
+      <Wrapper>
+        <Title>Assets</Title>
+          <ButtonsContainer>
+          <WalletLink>
+            <WalletLinkTitle>
+              Wallet connected
+            </WalletLinkTitle>
+            <WalletAddress>
+              {walletAddress.slice(0,7)} ... {walletAddress.slice(35)}
+            </WalletAddress>
+          </WalletLink>
+            <Button style={{ backgroundColor: '#3773f5', color: '#000',background: '#282b2f',fontweight: 'bold',letterspacing: '0.2rem',color: '#fff'}}>
+        Buy/Sell
+             </Button>
+         <Button style={{ backgroundColor: '#3773f5', color: '#000',background: '#282b2f',fontweight: 'bold',letterspacing: '0.2rem',color: '#fff'}}>
+        Send/Recieve
+          </Button>
+        </ButtonsContainer>
+      </Wrapper>
+      
+  )
+}
+
+export default Header
+
+
+const Wrapper = styled.div`
+ width: calc(100% - 3rem);
+  padding: 1rem 1.5rem;
+  border-bottom: 1px solid #282b2f;
+  display: flex;
+  align-items: center;
+  background: #000;
+  `
+
+const Title = styled.div` 
+  font-size: 2 rem;
+  font-weight: 600;
+  flex: 1;
+  fontColor: #0000FF;
+  `
+
+const ButtonsContainer = styled.div`
+display: flex;
+`
+const Button = styled.div`
+  border: 1px solid #282b2f;
+  padding: 0.8rem;
+  font-size: 1.3rem;
+  font-weight: 500;
+  border-radius: 0.4rem;
+  margin-right: 1rem;
+  &:hover {
+    cursor: pointer;
+  }
+`
+const WalletLink = styled.div`
+    font-size: 0.8rem;
+    border: 1px solid #282b2f;
+    border-radius: 50rem;
+    font-size: 1.2rem;
+    margin-right: 1rem;
+    padding: 0 1 rem;
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+
+`
+
+const WalletLinkTitle = styled.div`
+    font-size: 1.1rem;
+    margin-bottom: 0.3rem;
+    color: #27ad75;
+    font-weight: 600;
+`
+const WalletAddress = styled.div`
+    font-size: 0.8rem;
+    
+`
---
+++ static/coins.js
@@ -0,0 +1,25 @@
+import btcLogo from '../assets/btc.png'
+import ethLogo from '../assets/eth.png'
+
+export const coins = [
+  {
+    name: 'Bitcoin',
+    sign: 'BTC',
+    logo: btcLogo,
+    balanceUsd: 230.32,
+    balanceCoin: 6.35667736,
+    priceUsd: 5.32,
+    change: -4.74,
+    allocation: 41.89,
+  },
+  {
+    name: 'Ethereum',
+    sign: 'ETH',
+    logo: ethLogo,
+    balanceUsd: 1.2,
+    balanceCoin: 3.35664236,
+    priceUsd: 5.22,
+    change: 6.24,
+    allocation: 17.89,
+  },
+]
